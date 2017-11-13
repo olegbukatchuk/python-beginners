05f99789d35f9b85c46509663fa9f1e4e1b19b7d
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-13 12:02:34 +0300

    ok

---
+++ input.py
@@ -0,0 +1,9 @@
+# Инициализируем переменную значением, введенным пользователем
+user = input('I am Python. What is your name? : ')
+print('Welcome,', user)
+
+# Инициализируем еще одну переменную значением, введенным пользователем
+lang = input('Favorite programming language? : ')
+
+# Выводим строку и значение переменной
+print(lang, 'Is', 'Fun', sep='*', end='!\n')
