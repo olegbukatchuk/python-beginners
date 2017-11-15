256e163098bdf96769a3d024f4bae539bc881c73
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-15 17:16:10 +0300

    ok

---
+++ assign.py
@@ -0,0 +1,18 @@
+a = 8
+b = 4
+print('Assign Values:\t\t', 'a =', a, '\tb =', b)
+
+a += b
+print('Add & Assign:\t\t', 'a =', a, '(8 += 4)')
+
+a -= b
+print('Substract & Assign:\t', 'a =', a, '(12 - 4)')
+
+a *= b
+print('Multiply & Assign:\t', 'a =', a, (8 * 4))
+
+a /= b
+print('Divide & Assign:\t', 'a =', a, (32 / 4))
+
+a %= b
+print('Modulus & Assign:\t', 'a =', a, (8 % 4))
---
+++ comparison.py
@@ -0,0 +1,20 @@
+nil = 0
+
+num = 0
+
+max = 1
+
+cap = 'A'
+
+low = 'a'
+
+print('Equality :\t', nil, '==', num, nil == num)
+print('Equality :\t', cap, '==', low, cap == low)
+
+print('Inequality :\t', nil, '!=', max, nil != max)
+
+print('Greater :\t', nil, '>', max, nil > max)
+print('Lesser :\t', nil, '<', max, nil < max)
+
+print('MoreOrEqual :\t', nil, '>=', num, nil >= num)
+print('LessorEqual :\t', max, '<=' ,num, max <= num)
