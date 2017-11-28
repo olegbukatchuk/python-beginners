f6852d13f16a1d9ca4b1ce44b912c1e945e67541
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-28 18:45:38 +0300

    ok

---
+++ manipulate.py
@@ -0,0 +1,10 @@
+def display(s):
+    '''Выводим значение аргумента.'''
+    print( s )
+
+display(display.__doc__)
+display(r'C:\Program Files')
+display('\nHello' + ' Python')
+display('Python In Easy Steps\n' [7:])
+display('p' in 'Python')
+display('P' in 'Python')
