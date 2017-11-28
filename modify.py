a5d7a8b00019cf2e1458ce2d3b61d2df3af55fa7
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-28 20:58:43 +0300

    ok

---
+++ modify.py
@@ -0,0 +1,13 @@
+string = 'python in easy steps'
+
+print('\nCapitalized:\t', string.capitalize())
+print('\nTitled:\t', string.title())
+print('\nCentered:\t', string.center(30, '*'))
+
+print('\nUppercase:\t', string.upper())
+print('\nJoined:\t\t', string.join('**'))
+
+print('\nJustified:\t', string.rjust(30, '*'))
+
+print('\nReplaced:\t', string.replace('s', '*'))
+
