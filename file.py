dc9762c6e9370343a500cb7a6d88bcaeb6236494
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-29 12:16:09 +0300

    ok

--- file.py
+++ file.py
@@ -4,9 +4,7 @@ poem += 'Upon that little tent of blue\n'
 poem += 'Which prisoners call the sky\n'
 
 file = open('poem.txt', 'w')
-
 file.write(poem)
-
 file.close()
 
 file = open('poem.txt', 'r')
@@ -14,4 +12,8 @@ file = open('poem.txt', 'r')
 for line in file:
     print(line, end='')
 
+file.close()
+
+file = open('poem.txt', 'a')
+file.write('(Oscar Wilde)')
 file.close()
