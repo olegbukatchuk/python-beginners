dac0524ba655f0ecec01b0262f003c89c85719f4
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-29 22:41:02 +0300

    ok

---
+++ tk_window.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+
+window.title('Приложение')
+
+label = Label(window, text='Я уже кое-что умею на Python\'е!')
+
+label.pack(padx=200, pady=50)
+
+window.mainloop()
