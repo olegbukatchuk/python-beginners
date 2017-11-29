21584d6fc9bd848f5c3f9e3f0f65257d0fe14414
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-29 22:13:30 +0300

    ok

--- upload.py
+++ upload.py
@@ -7,3 +7,16 @@ filename = os.path.basename(upload.filename)
 
 with open(filename, 'wb') as copy:
     copy.write(upload.file.read())
+
+print('Content-type:text/html\r\n\r\n')
+print('<!DOCTYPE HTML>')
+print('<html lang="en">')
+print('<head>')
+print('<meta charset="UTF-8">')
+print('<title>Python Response</title>')
+print('</head>')
+print('<body>')
+print('<h1>File Uploaded:', filename, '</h1>')
+print('<a href="upload.html">Back</a>')
+print('</body>')
+print('</html>')
