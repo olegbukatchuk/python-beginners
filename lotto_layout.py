c6d9d64ddfd6b926be9a2e6902bbecc4ad0a34dc
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-01 00:12:39 +0300

    ok

--- lotto_layout.py
+++ lotto_layout.py
@@ -2,4 +2,4 @@ imgLbl.grid(row=1, column=1, rowspan=2)
 
 label1.grid(row=1, column=2, padx=10)
 label2.grid(row=1, column=3, padx=10)
-label3.grid( row = 1, column = 4, padx = 10 )
+label3.grid(row = 1, column = 4, padx = 10 )
