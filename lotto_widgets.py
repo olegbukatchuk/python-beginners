e367803bb9c3c2198ae165228e2ce04e979d3ddd
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-30 23:54:03 +0300

    ok

---
+++ lotto_widgets.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+
+img = PhotoImage(file='lotto.gif')
+
+imgLbl = Label(window, image=img)
+label1 = Label(window, relief='groove', width=2)
+label2 = Label(window, relief='groove', width=2)
+label3 = Label(window, relief='groove', width= 2 ) label4 = Label( window, relief = 'groove', width = 2 ) label5 = Label( window, relief = 'groove', width = 2 ) label6 = Label( window, relief = 'groove', width = 2 ) getBtn = Button( window )
+resBtn = Button( window )
