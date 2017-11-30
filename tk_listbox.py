f7ea6bf47dba4228e59efd7e4835490612e520e7
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-30 14:37:21 +0300

    ok

---
+++ tk_listbox.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+import tkinter.messagebox as box
+
+window = Tk()
+window.title('Listbox Example')
+
+frame = Frame(window)
+
+listbox = Listbox(frame)
+listbox.insert(1, 'HTML5 in easy steps')
+listbox.insert(2, 'CSS3 in easy steps')
+listbox.insert(3, 'JavaScript in easy steps')
+
+def dialog():
+    box.showinfo('Selection', 'Your Choice: ' + listbox.get(listbox.curselection()))
+
+btn = Button(frame, text='Choose', command=dialog)
+
+
