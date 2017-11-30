aad30154dd2289739faa04974a5c13ede660b955
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-01 00:46:53 +0300

    ok

--- lotto_widgets.py
+++ lotto_widgets.py
@@ -27,3 +27,16 @@ getBtn.grid()
 resBtn.grid()
 
 window.mainloop()
+
+imgLbl.grid(row=1, column=1, rowspan=2)
+
+label1.grid(row=1, column=2, padx=10)
+label2.grid(row=1, column=3, padx=10)
+label3.grid(row=1, column=4, padx=10)
+label4.grid(row=1, column=5, padx=10)
+label5.grid(row=1, column=6, padx=10)
+label6.grid(row=1, column=7, padx=(10, 20))
+
+getBtn.grid(row=2, column=2, columnspan=4)
+getBtn.grid(row=2, column=6, columnspan=2)
+
