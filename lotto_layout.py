ce841e7c581915efc4b03b1d762dd65a1932ff8e
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-01 00:42:28 +0300

    ok

--- lotto_layout.py
+++ lotto_layout.py
@@ -8,4 +8,4 @@ label5.grid(row=1, column=6, padx=10)
 label6.grid(row=1, column=7, padx=(10, 20))
 
 getBtn.grid(row=2, column=2, columnspan=4)
-getBtn.grid( row = 2, column = 6, columnspan = 2 )
+getBtn.grid(row = 2, column = 6, columnspan = 2 )
