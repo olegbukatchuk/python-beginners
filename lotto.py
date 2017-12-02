17306fe6ba71766eaaf7f137b45db04ed6a5b87a
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-02 15:58:44 +0300

    ok

--- lotto.py
+++ lotto.py
@@ -15,19 +15,6 @@ label6 = Label(window, relief='groove', width=2)
 getBtn = Button(window)
 resBtn = Button(window)
 
-imgLbl.grid()
-label1.grid()
-label2.grid()
-label3.grid()
-label4.grid()
-label5.grid()
-label6.grid()
-
-getBtn.grid()
-resBtn.grid()
-
-window.mainloop()
-
 imgLbl.grid(row=1, column=1, rowspan=2)
 label1.grid(row=1, column=2, padx=10)
 label2.grid(row=1, column=3, padx=10)
@@ -37,6 +24,50 @@ label5.grid(row=1, column=6, padx=10)
 label6.grid(row=1, column=7, padx=(10, 20))
 
 getBtn.grid(row=2, column=2, columnspan=4)
-getBtn.grid(row=2, column=6, columnspan=2)
+resBtn.grid(row=2, column=6, columnspan=2)
 
+window.title('Lotto Number Picker')
+window.resizable(0, 0)
 
+getBtn.configure(text='Get My Lucky Number')
+resBtn.configure(text='Reset')
+
+label1.configure(text='...')
+label2.configure(text='...')
+label3.configure(text='...')
+label4.configure(text='...')
+label5.configure(text='...')
+label6.configure(text='...')
+
+resBtn.configure(state=DISABLED)
+
+from random import sample
+
+def pick():
+    nums = sample(range(1, 49), 6)
+
+    label1.configure(text=nums[0])
+    label2.configure(text=nums[1])
+    label3.configure(text=nums[2])
+    label4.configure(text=nums[3])
+    label5.configure(text=nums[4])
+    label6.configure(text=nums[5])
+
+    getBtn.configure(state=DISABLED)
+    resBtn.configure(state=NORMAL)
+
+def reset():
+    label1.configure(text='...')
+    label2.configure(text='...')
+    label3.configure(text='...')
+    label4.configure(text='...')
+    label5.configure(text='...')
+    label6.configure(text='...')
+
+    getBtn.configure(state=NORMAL)
+    resBtn.configure(state=DISABLED)
+
+getBtn.configure(command=pick)
+resBtn.configure(command=reset)
+
+window.mainloop()
